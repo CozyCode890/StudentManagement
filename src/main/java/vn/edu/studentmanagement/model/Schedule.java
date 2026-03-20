ba6fdb8e9936fd464d6c803eee8b4f6efc0367a6
dongduongdang035 <duongdong.tvnk@gmail.com>
2026-03-20 15:13:26 +0700

    Add files via upload
    Duong Dong

---
+++ src/main/java/vn/edu/studentmanagement/model/Schedule.java
@@ -0,0 +1,36 @@
+package DSA_Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedule {
+  private String studentId;
+  private List<Course> selectedCourses = new ArrayList<>();
+
+  public Schedule() {
+  }
+
+  public Schedule(String studentId) {
+    this.studentId = studentId;
+  }
+
+  public String getStudentId() {
+    return studentId;
+  }
+
+  public void setStudentId(String studentId) {
+    this.studentId = studentId;
+  }
+
+  public List<Course> getSelectedCourses() {
+    return selectedCourses;
+  }
+
+  public void setSelectedCourses(List<Course> selectedCourses) {
+    this.selectedCourses = selectedCourses;
+  }
+
+  public int selectedCoursesCount() {
+    return selectedCourses == null ? 0 : selectedCourses.size();
+  }
+}
